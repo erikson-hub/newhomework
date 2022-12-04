e2e80e248d67896724ba1d3845c31057ae240c53
Ricardo J. Castillo López <87798579+rjcastillolopez@users.noreply.github.com>
2022-12-04 10:21:48 -0500

    lista empleados

--- sesion9/employee/employeedata.js
+++ sesion9/employee/employeedata.js
@@ -1,1 +1,18 @@
-import { users } from '../users/users';
+import { users } from "../users/users.js";
+
+// Obtener los datos del usuario seleccionado que fueron enviados desde index.html
+const user = JSON.parse(localStorage.getItem("user"));
+// Obtener los elementos del DOM
+const codigo = document.querySelector("#codigo");
+const nombre = document.querySelector("#nombre");
+const apellido = document.querySelector("#apellido");
+const correo = document.querySelector("#correo");
+const sueldobruto = document.querySelector("#sueldobruto");
+const sueldoneto = document.querySelector("#sueldoneto");
+// Asignar los valores del usuario seleccionado a los elementos del DOM
+codigo.textContent = user.codigo;
+nombre.textContent = user.nombre;
+apellido.textContent = user.apellido;
+correo.textContent = user.correo;
+sueldobruto.textContent = user.sueldobruto;
+sueldoneto.textContent = user.sueldoneto;
--- sesion9/list/list.js
+++ sesion9/list/list.js
@@ -1,96 +1,143 @@
-import { users } from '../users/users.js';
+import { users } from "../users/users.js";
+
+// Crear filas de la tabla
+function rows_table(users, tbody) {
+   users.forEach((user) => {
+      const tr = document.createElement("tr");
+      tbody.append(tr);
+      const keys = Object.keys(users[0]); // Atributos de los objetos
+      keys.push("datos");
+      keys.forEach((key) => {
+         const td = document.createElement("td");
+         if (key === "datos") {
+            const a = document.createElement("a");
+            a.classList.add("link");
+            a.textContent = "Ver datos ...";
+            a.href = "#";
+            td.append(a);
+         } else {
+            td.textContent = user[key];
+         }
+         tr.append(td);
+      });
+   });
+}
 
 // Tabla de usuarios
 function users_table(users) {
-  const tableUsers = document.getElementById('table-users');
-  tableUsers.innerHTML = ''; // Limpiar tabla
-  // leer encabezados
-  const keys = Object.keys(users[0]);
-  // crear tabla
-  const table = document.createElement('table');
-  table.classList.add('table');
-  // crear encabezados de la tabla
-  const thead = document.createElement('thead');
-  table.append(thead);
-  const tr = document.createElement('tr');
-  thead.append(tr);
-  keys.forEach((key) => {
-    const th = document.createElement('th');
-    th.textContent = key;
-    tr.append(th);
-  });
-  // crear cuerpo de la tabla
-  const tbody = document.createElement('tbody');
-  table.append(tbody);
-  users.forEach((user) => {
-    const tr = document.createElement('tr');
-    tbody.append(tr);
-    keys.forEach((key) => {
-      const td = document.createElement('td');
-      td.textContent = user[key];
-      tr.append(td);
-    });
-  });
-  tableUsers.append(table);
-  const br = document.createElement('br');
-  tableUsers.append(br);
+   const tableUsers = document.getElementById("table-users");
+   tableUsers.innerHTML = ""; // Limpiar tabla
+   // Crear tabla
+   const table = document.createElement("table");
+   table.classList.add("table");
+   tableUsers.append(table);
+   // Crear encabezados de la tabla
+   const headers = [
+      "Codigo",
+      "Nombre",
+      "Apellido",
+      "Correo",
+      "Sueldo bruto",
+      "Sueldo neto",
+      "Datos empleado",
+   ];
+   const thead = document.createElement("thead");
+   table.append(thead);
+   const tr = document.createElement("tr");
+   thead.append(tr);
+   headers.forEach((header) => {
+      const th = document.createElement("th");
+      th.id = "asc";
+      th.textContent = header;
+      tr.append(th);
+   });
+   // Crear cuerpo de la tabla (filas)
+   const tbody = document.createElement("tbody");
+   table.append(tbody);
+   rows_table(users, tbody);
 }
 
-// Funcion para leer usuarios
-function read(users) {
-  users_table(users);
+// Enviar datos de usuario cuando se hace click en el enlace "Ver datos ..."
+function send_user_data() {
+   const links = document.querySelectorAll(".link");
+   links.forEach((link) => {
+      link.addEventListener("click", (e) => {
+         e.preventDefault();
+         const tr = e.target.parentElement.parentElement; // link -> td -> tr (fila)
+         const tds = tr.querySelectorAll("td");
+         const user = {
+            codigo: tds[0].textContent,
+            nombre: tds[1].textContent,
+            apellido: tds[2].textContent,
+            correo: tds[3].textContent,
+            sueldobruto: tds[4].textContent,
+            sueldoneto: tds[5].textContent,
+         };
+         localStorage.setItem("user", JSON.stringify(user));
+         link.href = `../employee/index.html?codigo=${user.codigo}`; // redireccionar
+         window.location.href = link.href;
+      });
+   });
 }
 
-// Actualizar usuarios
-// function update() {
-//   const id = prompt_alerts(
-//     null,
-//     'id',
-//     'number',
-//     (value) => value > 0,
-//     'Ingrese un id valido'
-//   );
-//   if (id === undefined) {
-//     return;
-//   }
-//   const user = users.find((user) => user.id === id);
-//   console.log(user);
-//   if (user) {
-//     for (const prop in user) {
-//       if (prop === 'id' || prop === 'created_at') {
-//         continue;
-//       } else if (prop === 'edad') {
-//         const edad = prompt_alerts(
-//           null,
-//           prop,
-//           'number',
-//           (value) => value > 0 && value < 100,
-//           'La edad debe estar entre 0 y 100'
-//         );
-//         if (edad === undefined) {
-//           return;
-//         } else {
-//           user[prop] = edad;
-//         }
-//       } else if (prop === 'modified_at') {
-//         user[prop] = new Date().toISOString();
-//       } else {
-//         const value = prompt_alerts(null, prop, 'string', null, null);
-//         if (value === undefined) {
-//           return;
-//         } else {
-//           user[prop] = value;
-//         }
-//       }
-//     }
-//   } else {
-//     alert('Usuario no existe');
-//   }
-//   read(users);
-// }
+// Ordenar por atributo de forma ascendente o descendente
+function sortBy(users, prop, asc) {
+   if (prop === "nombre" || prop === "apellido" || prop === "correo") {
+      return users.sort((a, b) => {
+         if (asc) {
+            return a[prop].localeCompare(b[prop]);
+         } else {
+            return b[prop].localeCompare(a[prop]);
+         }
+      });
+   } else {
+      return users.sort((a, b) => {
+         if (asc) {
+            return a[prop] - b[prop];
+         } else {
+            return b[prop] - a[prop];
+         }
+      });
+   }
+}
+
+// Ordenar haciendo click en encabezados.
+function sort_by_headers(users) {
+   const ths = document.querySelectorAll("th");
+   ths.forEach((th) => {
+      if (th.textContent !== "Datos empleado") {
+         th.addEventListener("click", () => {
+            // Eliminar las clases de los encabezados previamente seleccionados
+            ths.forEach((th) => {
+               th.classList.remove("th-sort-asc");
+               th.classList.remove("th-sort-desc");
+            });
+            // Ordenar de acuerdo al encabezado seleccionado y el valor del atributo id
+            let prop = th.textContent.toLowerCase().replace(" ", "");
+            let sorted = null;
+            if (th.id === "asc") {
+               th.classList.add("th-sort-asc");
+               th.id = "desc";
+               sorted = sortBy(users, prop, true);
+            } else {
+               th.classList.add("th-sort-desc");
+               th.id = "asc";
+               sorted = sortBy(users, prop, false);
+            }
+            // Actualizar tabla
+            const tbody = document.querySelector("tbody");
+            tbody.innerHTML = ""; // limpiar tabla
+            rows_table(sorted, tbody);
+            send_user_data();
+         });
+      }
+   });
+}
 
 function main() {
-  read(users);
+   users_table(users);
+   sort_by_headers(users);
+   send_user_data();
 }
 
 main();
